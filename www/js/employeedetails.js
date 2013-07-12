66368b2b6cd836e0c48b86eea92255add55b1fde
nicola-c <nicola.cesaroni@gmail.com>
2013-07-12 07:41:15 +0200

    b6

--- www/js/employeedetails.js
+++ www/js/employeedetails.js
@@ -15,17 +15,9 @@ function onDeviceReady() {
 	
 
 	$('.scan').bind('click',function() {
-		//alert("click");
-		window.plugins.barcodeScanner.scan( function(result) {
-				alert("We got a barcode\n" +
-						  "Result: " + result.text + "\n" +
-						  "Format: " + result.format + "\n" +
-						  "Cancelled: " + result.cancelled);
-			}, function(error) {
-				alert("Scanning failed: " + error);
-						}
-			);
-	//	scanner.scan( function (result) { alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled); }, function (error) { alert("Scanning failed: " + error); } );	
+		var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+		alert(scanner);
+		scanner.scan( function (result) { alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled); }, function (error) { alert("Scanning failed: " + error); } );	
 	});	
 }
 
